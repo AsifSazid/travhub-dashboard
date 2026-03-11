3007e204b10d6f2f6b6ccd315dcd56950f39fdc3
AsifSazid <asif83sazid@gmail.com>
2026-03-11 10:44:14 +0600

    trying resolve error

--- api/tasks/store.php
+++ api/tasks/store.php
@@ -16,7 +16,7 @@ ini_set('display_errors', 1);
 // ---------------- CONFIGURATION ----------------
 // $GEMINI_API_KEY = "AIzaSyDtXWhpsUeWD6fLT8MeikxvgiPkynh2V0o"; // Replace with your actual API key
 $GEMINI_API_KEY = trim(file_get_contents('../../gemini-apikey.txt')); // Replace with your actual API key
-$SERVER_CUS_PATH = trim(file_get_contents('../../server-name.txt')); // Server Naming 
+$SERVER_CUS_PATH = trim(file_get_contents('../../staging.txt')); // Replace with your actual API key
 $GEMINI_MODEL = "gemini-2.0-flash-lite";
 
 
@@ -76,8 +76,8 @@ $workSysId   = preg_replace('/\s+/u', '', $work['sys_id']);
 $workTitle   = preg_replace('/\s+/u', '_', $work['title']);         
 
 // Build folder path
-$clientFolder = "{$SERVER_CUS_PATH}-clients/{$clientSysId}_{$clientName}/{$workSysId}"."+"."$workTitle";
-$clientFolderName = "{$SERVER_CUS_PATH}-clients/{$clientSysId}_{$clientName}/{$workSysId}"."+"."$workTitle/tasks";
+$clientFolder = "dev-clients/{$clientSysId}_{$clientName}/{$workSysId}"."+"."$workTitle";
+$clientFolderName = "dev-clients/{$clientSysId}_{$clientName}/{$workSysId}"."+"."$workTitle/tasks";
 $taskDirectory = makeDir($clientFolderName, $cleanSysId);
 makeSMBDir($clientFolder, 'tasks');
 $fullPath = makeSMBDir($clientFolderName, $cleanSysId);
